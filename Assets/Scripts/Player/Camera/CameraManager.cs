c76a4251e582480111c478b10ebb59d2e55ab111
Max JC <maxjamiesoncolmer@gmail.com>
2025-04-14 13:55:31 +0930

    Added potential skybox and obstacles in TestScene

---
+++ Assets/Scripts/Player/Camera/CameraManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // Manages the player's camera
+
+    [Tooltip("The player's camera")]
+    [SerializeField] Camera playerCam;
+
+
+}
