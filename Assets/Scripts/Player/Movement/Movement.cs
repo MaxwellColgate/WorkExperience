42e6adf330d7476d2ae51c764c453eb95f7b1544
Max JC <maxjamiesoncolmer@gmail.com>
2025-04-16 14:14:12 +0930

    Continued player movement refinements, time to playtest

--- Assets/Scripts/Player/Movement/Movement.cs
+++ Assets/Scripts/Player/Movement/Movement.cs
@@ -11,9 +11,15 @@ public class Movement : MonoBehaviour
     [SerializeField] CameraManager cameraMan;
 
     public float forwardforce = 300;
+
+    [Tooltip("How long you have to wait (in seconds) after turning before you can turn again")]
+    public float waitBetweenTurns = 0.2f;
+
     public float jumpforce = 5f;
 
-    public bool isGrounded;
+    public bool isGrounded; // Is the player grounded?
+
+    float currentTurnWait; // How long the player has waited since their last turn
 
     private void Start()
     {
@@ -26,25 +32,31 @@ public class Movement : MonoBehaviour
         body.velocity = new Vector3(transform.forward.x * forwardforce * Time.deltaTime, body.velocity.y, transform.forward.z * forwardforce * Time.deltaTime);
 
         //jump input
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
+        if (Input.GetKey(KeyCode.Space) && isGrounded) 
         {
             isGrounded = false;
+            body.velocity = new Vector2(body.velocity.x, 0);
             body.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
         }
 
         //Rotate left (A key)
-        if (Input.GetKeyDown(KeyCode.A)) 
+        if (Input.GetKeyDown(KeyCode.A) && currentTurnWait >= waitBetweenTurns) 
         {
             transform.Rotate(0, -90f, 0);
             cameraMan.SwitchCameraPos(CameraManager.cameraPositions.defaultLeft, 0.02f);
+            currentTurnWait = 0;
         }
 
         //Rotate Right (D key )
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D) && currentTurnWait >= waitBetweenTurns)
         {
             transform.Rotate(0, 90f, 0);
             cameraMan.SwitchCameraPos(CameraManager.cameraPositions.defaultRight, 0.02f);
+            currentTurnWait = 0;
         }
+
+        // If the player is recharging their turn, continue the recharge
+        if(currentTurnWait < waitBetweenTurns) { currentTurnWait += Time.deltaTime; }
 
     }
 }
